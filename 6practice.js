5566d706ba756627c51637cf8424e725c5859cee
Christopher O'Bannon <Crobwan@gmail.com>
2019-07-21 16:27:01 -0500

    added hmtl and jquery on synposis exercise

--- 6practice.js
+++ 6practice.js
@@ -1,1 +1,29 @@
-console.log("i love you");
+$( document ).ready(readyNow);
+
+const budget = 25000;
+let purchases = [];
+
+function addPurchase(){
+console.log('in addPurchase');
+// get user input create a new object
+let newPurchase = {
+  name: $('#purchaseNameIn').val(),
+  price: $('#purchasePriceIn').val(),
+}
+// push the new purchase into the array
+purchases.push(newPurchase);
+// empty the inputs
+$('#purchaseNameIn').val('');
+$('#purchasePriceIn').val('');
+//calculate remainingBudget
+} // end addPurchase
+
+function readyNow(){
+  //Display the budget
+  // target budgetOut by ID
+  let el = $('#budgetOut');
+  el.empty();
+  el.append( budget);
+  // handle click event
+  $('#addPurchaseButton').on( 'click',addPurchase );
+}// end readyNow
