8ad47299dbda64a034a969a1319be69f0098b3e4
Christopher O'Bannon <Crobwan@gmail.com>
2019-07-21 15:05:31 -0500

    some practice

--- week6.js
+++ week6.js
@@ -1,1 +1,18 @@
 console.log("Yo");
+
+
+$ ( document ).ready( readyNow );
+
+function readyNow (){
+  console.log("JQ");
+  $ ('#sayHelloButton').on( 'click', sayHello )
+} // end readyNow
+
+function sayHello(){
+  // target the input element by ID
+  // get the value of the text input
+  let userName = $('#nameInput').val();
+  console.log( 'Hello', userName );
+  // empty out the input field
+  $('#nameInput').val('');
+}// end sayHello
