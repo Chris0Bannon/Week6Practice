167bf4f8a78c59968006698a9ab805a44b2a6969
Christopher O'Bannon <Crobwan@gmail.com>
2019-07-21 17:24:13 -0500

    finished the exercise, budget app is functional

--- 6practice.js
+++ 6practice.js
@@ -16,7 +16,42 @@ purchases.push(newPurchase);
 $('#purchaseNameIn').val('');
 $('#purchasePriceIn').val('');
 //calculate remainingBudget
+calculateRemainingBudget()
 } // end addPurchase
+
+function calculateRemainingBudget(){
+  console.log('in calculateRemainingBudget');
+  // loop through the purchases array
+  let totalPrices = 0;
+  for (var i = 0; i < purchases.length; i++) {
+    // for each purchase add up total of all prices
+    totalPrices += Number(purchases[i].price);
+  } // end for loop
+  console.log('totalPrices', totalPrices );
+  // subtract total prices from the budget remainingBudget
+const remainingBudget = budget - totalPrices;
+  //display remainingBudget
+  let el = $('#remainingBudgetOut');
+  el.empty();
+  el.append(remainingBudget);
+  //update DOM
+  displayPurchases();
+}// end calculateRemainingBudget
+
+function displayPurchases(){
+  console.log('displayPurchases');
+  //target our ouput by its id
+  let el = $('#PurchasesOut');
+  //empty
+  el.empty();
+  //loop through purchases array
+  for (purchase of purchases) {
+    // for each purchase, create a list item
+    el.append('<li>' + purchase.name + ': $' + purchase.price + '</li>');
+
+  }// end for
+
+}// end displayPurchases
 
 function readyNow(){
   //Display the budget
@@ -26,4 +61,6 @@ function readyNow(){
   el.append( budget);
   // handle click event
   $('#addPurchaseButton').on( 'click',addPurchase );
+  //init Display
+  calculateRemainingBudget();
 }// end readyNow
